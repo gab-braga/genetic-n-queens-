83ece14b3ef88e2c6467b21b520df62ee83be506
gab-braga <gabrielbraga0712@gmail.com>
2024-08-27 19:19:55 -0300

    fix: update services

--- js/script.js
+++ js/script.js
@@ -1,56 +1,21 @@
-import { generateInitialPopulation } from "/js/service.js";
+import {
+    generateInitialPopulation,
+    formatHtmlForBoard
+} from "/js/service.js";
+
+const SIZE_BOARD = 8
 
 const btn = document.querySelector("#btn");
 
 btn.addEventListener("click", () => {
     const root = document.querySelector("#root");
     const size = document.querySelector("#size").value || 0;
-    const population  = generateInitialPopulation(size);
+    const population  = generateInitialPopulation(size, SIZE_BOARD);
     let contentRoot = "";
-    population.forEach(popu => {
-        contentRoot += genereateHtmlForBoard(popu);
+    population.forEach(queens => {
+        contentRoot += formatHtmlForBoard(queens);
     })
     root.innerHTML = contentRoot;
 
 })
 
-function genereateHtmlForBoard(queens) {
-    let html = "<table class='board'>";
-    const board = generateBoardMap(queens);
-    let size = queens.length;
-    for(let y = 0; y < size; y++) {
-        html += "<tr>";
-        for(let x = 0; x < size; x++) {
-            if(board[y][x]) {
-                html += "<td class='queen'>";
-            } else {
-                html += "<td>";
-            }
-            html += "</td>";
-        }
-        html += "</tr>";
-    }
-    html += "</table>";
-    return html;
-}
-
-function generateEmptyBoardMap(size) {
-    let board = [];
-    for(let y = 0; y < size; y++) {
-        let row = [];
-        for(let x = 0; x < size; x++) {
-            row.push(false);
-        }
-        board.push(row);
-    }
-    return board;
-}
-
-function generateBoardMap(queens) {
-    let size = queens.length;
-    const board = generateEmptyBoardMap(size);
-    queens.forEach(qn => {
-        board[qn.y][qn.x] = true;
-    })
-    return board;
-}
--- js/service.js
+++ js/service.js
@@ -1,13 +1,39 @@
-const SIZE_BOARD = 8
+function generateInitialPopulation(populationSize, boardSize) {
+    let population = [];
+    for(let i = 0; i < populationSize; i++) {
+        let individual = generateQueens(boardSize)
+        population.push(individual)
+    }
+    return population;
+}
 
-function getRandInt(min, max) {
-    return Math.floor(Math.random() * (1 + max - min)) + min;
+function generateQueens(size) {
+    const queens = [];
+    let cols = generateCols(size);
+    for(let x = 0; x < size; x++) {
+        let y = choiceInList(cols);
+        queens.push({ x, y });
+        cols = removeInList(y, cols)
+    }
+    return queens;
+}
+
+function generateCols(size) {
+    const cols = [];
+    for(let idx = 0; idx < size; idx++) {
+        cols.push(idx);
+    }
+    return cols;
 }
 
 function choiceInList(list) {
     let size = list.length;
     let index = getRandInt(0, size - 1);
     return list[index];
+}
+
+function getRandInt(min, max) {
+    return Math.floor(Math.random() * (1 + max - min)) + min;
 }
 
 function removeInList(elem, list) {
@@ -17,32 +43,44 @@ function removeInList(elem, list) {
     return clone;
 }
 
-function generateInitialPopulation(size) {
-    let population = [];
-    for(let i = 0; i < size; i++) {
-        let individuo = generateQueens()
-        population.push(individuo)
+function formatHtmlForBoard(queens) {
+    let html = "<table class='board'>";
+    const board = generateBoardMap(queens);
+    const size = queens.length;
+    for(let y = 0; y < size; y++) {
+        html += "<tr>";
+        for(let x = 0; x < size; x++) {
+            if(board[y][x]) {
+                html += "<td class='queen'>";
+            } else {
+                html += "<td>";
+            }
+            html += "</td>";
+        }
+        html += "</tr>";
     }
-    return population;
+    html += "</table>";
+    return html;
 }
 
-function generateQueens() {
-    const queens = [];
-    let cols = generateCols();
-    for(let x = 0; x < SIZE_BOARD; x++) {
-        let y = choiceInList(cols);
-        queens.push({ x, y });
-        cols = removeInList(y, cols)
-    }
-    return queens;
+function generateBoardMap(queens) {
+    let size = queens.length;
+    const board = generateEmptyBoardMap(size);
+    queens.forEach(({y, x}) => {
+        board[y][x] = true;
+    })
+    return board;
 }
 
-function generateCols() {
-    const cols = [];
-    for(let idx = 0; idx < SIZE_BOARD; idx++) {
-        cols.push(idx);
+function generateEmptyBoardMap(size) {
+    let board = [];
+    for(let y = 0; y < size; y++) {
+        let row = [];
+        for(let x = 0; x < size; x++)
+            row.push(false);
+        board.push(row);
     }
-    return cols;
+    return board;
 }
 
-export { generateInitialPopulation }
+export { generateInitialPopulation, formatHtmlForBoard }
