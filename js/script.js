974cdd6645a45cb54f3c54e3b8ac6e7faf5a5bd5
gab-braga <gabrielbraga0712@gmail.com>
2024-08-27 16:21:33 -0300

    feat: created function to viewer boards

--- js/script.js
+++ js/script.js
@@ -1,10 +1,56 @@
 import { generateInitialPopulation } from "/js/service.js";
 
 const btn = document.querySelector("#btn");
-const root = document.querySelector("#root");
 
 btn.addEventListener("click", () => {
+    const root = document.querySelector("#root");
     const size = document.querySelector("#size").value || 0;
     const population  = generateInitialPopulation(size);
-    console.log(population)
+    let contentRoot = "";
+    population.forEach(popu => {
+        contentRoot += genereateHtmlForBoard(popu);
+    })
+    root.innerHTML = contentRoot;
+
 })
+
+function genereateHtmlForBoard(queens) {
+    let html = "<table class='board'>";
+    const board = generateBoardMap(queens);
+    let size = queens.length;
+    for(let y = 0; y < size; y++) {
+        html += "<tr>";
+        for(let x = 0; x < size; x++) {
+            if(board[y][x]) {
+                html += "<td class='queen'>";
+            } else {
+                html += "<td>";
+            }
+            html += "</td>";
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+    return html;
+}
+
+function generateEmptyBoardMap(size) {
+    let board = [];
+    for(let y = 0; y < size; y++) {
+        let row = [];
+        for(let x = 0; x < size; x++) {
+            row.push(false);
+        }
+        board.push(row);
+    }
+    return board;
+}
+
+function generateBoardMap(queens) {
+    let size = queens.length;
+    const board = generateEmptyBoardMap(size);
+    queens.forEach(qn => {
+        board[qn.y][qn.x] = true;
+    })
+    return board;
+}
